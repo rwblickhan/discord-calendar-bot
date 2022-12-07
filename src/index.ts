41df97a5ba672d17206051192b6922de773c5a3b
Russell Blickhan <rwblickhan@gmail.com>
2022-12-07 15:23:29 -0800

    Set cron expression

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,7 @@
  * Learn more at https://developers.cloudflare.com/workers/runtime-apis/scheduled-event/
  */
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-}
+export interface Env {}
 
 export default {
 	async scheduled(
