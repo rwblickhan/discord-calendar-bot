afe68ea663575e2b1832ec9c2642da80fbf043fa
Russell Blickhan <rwblickhan@gmail.com>
2022-12-07 22:06:14 -0800

    Set up sentry

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,13 @@
  * Learn more at https://developers.cloudflare.com/workers/runtime-apis/scheduled-event/
  */
 
+import { Toucan } from "toucan-js";
+
 export interface Env {
     DISCORD_API_TOKEN: string;
     DISCORD_GUILD_ID: string;
     DISCORD_ANNOUNCEMENTS_CHANNEL_ID: string;
+    SENTRY_DSN: string;
 }
 
 export interface ScheduledEvent {
@@ -30,21 +33,39 @@ export default {
         ctx: ExecutionContext
     ): Promise<void> {
         console.log(`Starting the bot...`);
+        const sentry = new Toucan({
+            dsn: env.SENTRY_DSN,
+        });
+        sentry.addBreadcrumb({
+            message: "Loading environment variables...",
+            category: "log",
+        });
         if (
             env.DISCORD_API_TOKEN === null ||
             env.DISCORD_API_TOKEN.length === 0
         ) {
-            throw new Error(`Failed to find Discord API token`);
+            const message = "Failed to find Discord API token";
+            sentry.captureException(message);
+            throw new Error(message);
         }
         if (
             env.DISCORD_GUILD_ID === null ||
             env.DISCORD_GUILD_ID.length === 0
         ) {
-            throw new Error(`Failed to find Discord guild ID`);
+            const message = "Failed to find Discord guild ID";
+            sentry.captureException(message);
+            throw new Error(message);
         }
         if (env.DISCORD_ANNOUNCEMENTS_CHANNEL_ID === null) {
-            throw new Error(`Failed to find Discord announcements channel ID`);
+            const message = "Failed to find Discord announcements channel ID";
+            sentry.captureException(message);
+            throw new Error(message);
         }
+
+        sentry.addBreadcrumb({
+            message: "Requesting events...",
+            category: "log",
+        });
 
         const scheduled_events_url = `https://discord.com/api/v10/guilds/${env.DISCORD_GUILD_ID}/scheduled-events`;
 
@@ -56,6 +77,11 @@ export default {
         });
         if (!scheduled_events_response.ok) {
             const error = await scheduled_events_response.text();
+            sentry.addBreadcrumb({
+                message: error,
+                category: "error",
+            });
+            sentry.captureException(scheduled_events_response);
             throw new Error(`Failed to get scheduled events: ${error}`);
         }
         const scheduled_events_json =
@@ -74,7 +100,7 @@ export default {
                 tomorrow.getDate() == event_date.getDate()
             ) {
                 console.log(`Posting message for event ${event.name}`);
-                return this.message(event, env);
+                return this.message(event, sentry, env);
             } else {
                 console.log(
                     `Skipping event ${event.name} since it's not tomorrow`
@@ -83,7 +109,15 @@ export default {
         });
         await Promise.all(messages);
     },
-    async message(event: ScheduledEvent, env: Env): Promise<void> {
+    async message(
+        event: ScheduledEvent,
+        sentry: Toucan,
+        env: Env
+    ): Promise<void> {
+        sentry.addBreadcrumb({
+            message: `Sending message for ${event.name}...`,
+            category: "log",
+        });
         const message_announcements_url = `https://discord.com/api/v10/channels/${env.DISCORD_ANNOUNCEMENTS_CHANNEL_ID}/messages`;
         let content = `"${event.name}" is coming up tomorrow!`;
         if (event.description != null) {
@@ -101,7 +135,12 @@ export default {
         });
         if (!message_response.ok) {
             const error = await message_response.text();
-            throw new Error(`Failed to get post message: ${error}`);
+            sentry.addBreadcrumb({
+                message: error,
+                category: "error",
+            });
+            sentry.captureException(message_response);
+            throw new Error(`Failed to post message: ${error}`);
         }
     },
 };
